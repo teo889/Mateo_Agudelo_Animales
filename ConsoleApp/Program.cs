d590829744582eeadd343485c20438eb8a9498a7
Mateo <mateoagudelo1117146@correo.itm.edu.co>
2025-08-28 07:38:27 -0500

    commit a rama 7:40

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,5 +1,43 @@
 ﻿public class Animales
 {
+    public int Id = 0;
+    public string? Nombre = "";
+    public int Edad = 0;
+
+    public int peso = 0;
+
+    public tipos Tipo = new tipos();
+    public colores Colores = new colores();
+    public sexos Sexos = new sexos();
+
+    public list<sexos> Sexos = new list<sexos>();
     
 }
+
+
+public class tipos
+{
+    public int Id = 0;
+    public string? Nombre ="";
+
+    public list<Animales> Animales = new list<Animales>();
+}
+
+
+public class colores
+{
+    public int Id = 0;
+    public string? Nombre ="";
+
+    public list<Animales> Animales = new list<Animales>();
+}
+
+public class sexos
+{
+    public int Id = 0;
+    public string? Nombre ="";
+}   
+
+
+
 
